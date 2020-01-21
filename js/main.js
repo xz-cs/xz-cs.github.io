0aef8a925bb14d69e40e7e02b8a12e48b0bada86
Xun Zhang <59300821+xz-cs@users.noreply.github.com>
2020-01-21 22:17:19 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,18 +16,28 @@ function show_hide() {
     }
 }*/
 
-let index=1;
+let index = 1;
+let keyword = String(window.location).split("?")[1];
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("nav").hide();
-    $("#content").animate({width:"96%"},888);
-    $(".container").click(function(){
+    $("#content").animate({width: "96%"}, 888);
+    $(".container").click(function () {
         $("nav").toggle(888);
-        if (index===0){
-            $("#content").animate({width:"96%"},888);
-        }else {
-            $("#content").animate({width:"86%"},888);
+        if (index === 0) {
+            $("#content").animate({width: "96%"}, 888);
+        } else {
+            $("#content").animate({width: "86%"}, 888);
         }
-        index=1-index;
+        index = 1 - index;
     });
 });
+
+function init() {
+    if (keyword!==undefined && keyword!==""){
+        document.querySelector("iframe").src = "html/" + keyword + ".html";
+    }
+    else{
+        document.querySelector("iframe").src = "html/home.html";
+    }
+}
